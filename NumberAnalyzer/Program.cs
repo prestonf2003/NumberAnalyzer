7bf47375974006c4047c9dc27a80f2c42714b53e
prestonf2003 <prestonfernando@gmail.com>
2022-04-12 15:04:39 -0400

    Optimized Code with nesting if statements

--- NumberAnalyzer/Program.cs
+++ NumberAnalyzer/Program.cs
@@ -4,17 +4,21 @@
     public static void Main()
     {
          
-        while(Loop == true)
+        while(true)
         {
             Console.WriteLine($"Hello Please input your name below:");
+                
                 string name = Console.ReadLine();
+            
             Console.WriteLine("Please input an integer between 0 and 100");
-            int num = int.Parse(Console.ReadLine());
-            bool even;
-            if(num > 0 && num < 100)
+            
+                int num = int.Parse(Console.ReadLine());
+            if (num > 0 && num < 100)
             {
                 Console.WriteLine("Great Job! Your Number is in the parameters");
             }
+
+            bool even;
             if (num % 2 == 0)
             {
                 even = true;
@@ -23,37 +27,52 @@
             {
                 even = false;
             }
-            if (num <= 60 && even == false)
+            if(even == true)
             {
-                Console.WriteLine($"{name} your number {num} is Odd and less than 60");
+                if (num >= 2 && num < 25)
+                {
+                    Console.WriteLine($"{name} your number is Even and less than 25");
+                }
+                else if (num < 60)
+                {
+                    Console.WriteLine($"{name} your number is Even and between 26 and 60 inclusive");
+                }
+                else
+                {
+                    Console.WriteLine($"{name} Your number {num} is even and greater than 60");
+                }
             }
-            else if (num >= 2 && num <= 24 && even == true)
+            if(even == false)
             {
-                Console.WriteLine($"{name} Even and less than 25");
+                if(num <=60)
+                {
+                    Console.WriteLine($"{name} your number {num} is Odd and less than 60");
+                }
+                else if(num >=60){
+                    Console.WriteLine($"{name} your number {num} is odd and greater than 60");
+                }
             }
-            else if (num >= 26 && num <= 60 && even == true)
-            {
-                Console.WriteLine($"{name} Even and between 26 and 60 inclusive");
-            }
-            else if (num > 60 && even == true)
-            {
-                Console.WriteLine($"{name} Your number {num} is even and greater than 60");
-            }
-            else if (num > 60 && even == false)
-            {
-                Console.WriteLine($"{name} Your number {num} is odd and greater than 60");
-            }
-            Console.WriteLine($"Would you like to continue? ");
+       
+           
+               
+        
+            
+
+            
+            Console.WriteLine($"Would you like to continue yes or no? ");
+            
             string cont = Console.ReadLine().ToLower();
+            
+            
             if(cont == "yes")
             {
-                Loop = true;
+                continue;
             }
             else
             {
-                Loop = false;
+               return;
             }
-
+ 
             
         }
 
